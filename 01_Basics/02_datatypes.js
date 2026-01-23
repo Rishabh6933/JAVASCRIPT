b3aadac0a141841a789ff2ce525b17efe8f0fa21
Rishabh Sharma <coderishabh.7106@gmail.com>
2026-01-23 23:03:24 +0530

    add account variables and data type examples

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all JS code as newer version
+
+// alert (3+3) // we are using node.js, not browser
+
+
+console.log(3+3)
+console.log("Rishabh")
+
+
+let name = "Rishabh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number --> 2^53
+// bigint --> for big number
+// string --> ""
+// boolean --> true/false
+// null --> standalone value
+// undefined --> value not assigned
+// symbol --> unique
+
+
+// object
+
+console.log(typeof undefined)   // undefined
+console.log(typeof null)        // object
