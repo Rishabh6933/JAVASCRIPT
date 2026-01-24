9fb19de1c0ac94e08b87fc3f86ec3a71463234c6
Rishabh Sharma <coderishabh.7106@gmail.com>
2026-01-24 07:53:05 +0530

    add datatypes summary with examples of primitive and reference types

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,48 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Javascript is dynamically typed language, it means
+// that variables types are determined at runtime
+
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 34565564465354531894n
+
+//console.log(id === anotherId);
+
+
+// Reference (Non-Primitive)
+
+// Array, Object, Functions
+
+
+const heroes = ["Shaktiman", "Nagraj", "Doga"];
+let myObj = {
+    name: "Rishabh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof myFunction);     // function
+// console.log(typeof heroes);         // object
+// console.log(typeof outsideTemp);    // object
+// console.log(typeof myObj);          // object
+// console.log(typeof id);             // symbol
+// console.log(typeof anotherId);      // symbol
+// console.log(typeof bigNumber);      // bigint
+
