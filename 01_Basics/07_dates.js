b2f52ef3ba2f93b015e8d035d0b3fdf52b657c1a
Rishabh Sharma <coderishabh.7106@gmail.com>
2026-01-24 15:58:31 +0530

    refactor: organize date-related code and comment out console log statements for clarity

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,37 @@
+// DATES
+
+let myDate = new Date()
+// console.log(typeof myDate);
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)  // months starts from 0 --> Jan, 1 --> Feb and so on.....
+// let myCreatedDate = new Date("2023-01-14")       // in this format the months starts from Jan, Feb and so on....
+let myCreatedDate = new Date("01-14-2025")       
+
+// console.log(myCreatedDate.toLocaleString())
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+
+
