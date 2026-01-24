5cb4b78020b5a4986b14d8e798bf42d1fcd1f047
Rishabh Sharma <coderishabh.7106@gmail.com>
2026-01-25 01:37:59 +0530

    refactor: comment out unused console log statements for clarity and maintainability

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -32,8 +32,36 @@ const obj4 = {5: "a", 6: "b"}
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Rishabh"
+}
+
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(instructor)
+
+
+
+// {
+//     name: "rishabh",
+//     price: "free",
+//     coursename: "js in hindi"
+// }
+
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
